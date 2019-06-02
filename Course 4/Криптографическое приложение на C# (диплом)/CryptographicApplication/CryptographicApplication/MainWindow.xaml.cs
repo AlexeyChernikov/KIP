f4c88a4405b7cfafea74f6e216b8c4d371d0af76
AlexeyChernikov <alex0899@yandex.ru>
2019-06-03 01:38:34 +0300

    Допилил RSA

--- Course 4/Криптографическое приложение на C# (диплом)/CryptographicApplication/CryptographicApplication/MainWindow.xaml.cs
+++ Course 4/Криптографическое приложение на C# (диплом)/CryptographicApplication/CryptographicApplication/MainWindow.xaml.cs
@@ -14,8 +14,6 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using Microsoft.Win32;
-using System.Numerics;
-using System.Diagnostics;
 
 namespace CryptographicApplication
 {
@@ -27,12 +25,12 @@ namespace CryptographicApplication
         
         #region Переменные
 
-        public char[] lang = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%^&*()+=-_'?.,|/`~№:;@[]{}\\".ToCharArray();
         Transposition trans_obj;
         Monoalphabetic mono_obj;
         Polyalphabetic poly_obj;
         XOR xor_obj;
         Vernam vernam_obj;
+        RSA rsa_obj;
 
         #endregion
 
@@ -45,6 +43,7 @@ namespace CryptographicApplication
             poly_obj = new Polyalphabetic();
             xor_obj = new XOR();
             vernam_obj = new Vernam();
+            rsa_obj = new RSA();
         }
 
         #region Функционал
@@ -101,122 +100,19 @@ namespace CryptographicApplication
 
         public void RSA_Cipher()
         {
-            long p = Convert.ToInt64(tb_Key.Text);
-            long q = Convert.ToInt64(tb_Key_2.Text);
 
-            if (IsTheNumberSimple(p) && IsTheNumberSimple(q))
+
+            if (rb_Encryption.IsChecked == true)
             {
-                string sourcetext = tb_SourceData.Text;
-
-                long n = p * q;
-                long m = (p - 1) * (q - 1);
-                long d = Calculate_d(m);
-                long e_ = Calculate_e(d, m);
-
-                List<string> result = RSA_Endoce(sourcetext, e_, n);
-
-                MessageBox.Show(d.ToString());
-                MessageBox.Show(n.ToString());
-
-                /*StreamWriter sw = new StreamWriter("out1.txt");
-                foreach (string item in result)
-                    sw.WriteLine(item);
-                sw.Close();
-
-                Process.Start("out1.txt");*/
+                tb_EncryptedData.Text = rsa_obj.Encrypt(tb_SourceData.Text, tb_Key.Text, tb_Key_2.Text);
             }
             else
-                MessageBox.Show("p или q - не простые числа!");
-        }
-
-        private bool IsTheNumberSimple(long n) //является ли число простым
-        {
-            if (n < 2)
-                return false;
-
-            if (n == 2)
-                return true;
-
-            for (long i = 2; i < n; i++)
-                if (n % i == 0)
-                    return false;
-
-            return true;
-        }
-
-        private long Calculate_e(long d, long m) //вычисление открытой экспоненты
-        {
-            long e = 10;
-
-            while (true)
             {
-                if ((e * d) % m == 1)
-                    break;
-                else
-                    e++;
+                String[] s = tb_SourceData.Text.Split(new String[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                List<string> sourcetext = new List<string>(s);
+
+                tb_EncryptedData.Text = rsa_obj.Decrypt(sourcetext, tb_Key.Text, tb_Key_2.Text);
             }
-
-            return e;
-        }
-
-        private long Calculate_d(long m) //вычисление закрытой экспоненты
-        {
-            long d = m - 1;
-
-            for (long i = 2; i <= m; i++)
-                if ((m % i == 0) && (d % i == 0)) //если имеют общие делители
-                {
-                    d--;
-                    i = 1;
-                }
-
-            return d;
-        }
-
-        private List<string> RSA_Endoce(string s, long e, long n) //шифрование
-        {
-            List<string> result = new List<string>();
-
-            BigInteger bi;
-
-            for (int i = 0; i < s.Length; i++)
-            {
-                int index = Array.IndexOf(lang, s[i]);
-
-                bi = new BigInteger(index);
-                bi = BigInteger.Pow(bi, (int)e);
-
-                BigInteger n_ = new BigInteger((int)n);
-
-                bi = bi % n_;
-
-                result.Add(bi.ToString());
-            }
-
-            return result;
-        }
-
-        private string RSA_Dedoce(List<string> input, long d, long n) //дешифрование
-        {
-            string result = "";
-
-            BigInteger bi;
-
-            foreach (string item in input)
-            {
-                bi = new BigInteger(Convert.ToDouble(item));
-                bi = BigInteger.Pow(bi, (int)d);
-
-                BigInteger n_ = new BigInteger((int)n);
-
-                bi = bi % n_;
-
-                int index = Convert.ToInt32(bi.ToString());
-
-                result += lang[index].ToString();
-            }
-
-            return result;
         }
 
         #endregion
@@ -415,10 +311,10 @@ namespace CryptographicApplication
 
         private void Without_a_Space(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Space)
+            /*if (e.Key == Key.Space)
             {
                 e.Handled = true;
-            }
+            }*/
         }
 
         #endregion
---
+++ Course 4/Криптографическое приложение на C# (диплом)/CryptographicApplication/CryptographicApplication/RSA.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.IO;
+using System.Windows;
+
+namespace CryptographicApplication
+{
+    class RSA
+    {
+        Alphabet alph = new Alphabet();
+
+        //дописать функцию генерации ключей
+
+        public string Encrypt(string sourcetext, string key_p, string key_q) //генерация ключа
+        {
+            StringBuilder code = new StringBuilder();
+
+            long p = Convert.ToInt64(key_p);
+            long q = Convert.ToInt64(key_q);
+
+            if (IsTheNumberSimple(p) && IsTheNumberSimple(q))
+            {
+                long n = p * q;
+                long fi = (p - 1) * (q - 1);
+                long e = Calculate_e(fi);
+                long d = Calculate_d(e, fi);
+
+                List<string> result = RSA_Endoce(sourcetext, e, n);
+
+                /*MessageBox.Show(Convert.ToString(e));
+                MessageBox.Show(Convert.ToString(n));
+                MessageBox.Show(Convert.ToString(d));
+                MessageBox.Show(Convert.ToString(n));*/
+
+                foreach (string item in result)
+                    code.Append(item + "\n");
+            }
+
+            return code.ToString();
+        }
+
+        public string Decrypt(List<string> sourcetext, string key_d, string key_n)
+        {
+            StringBuilder code = new StringBuilder();
+
+            long d = Convert.ToInt64(key_d);
+            long n = Convert.ToInt64(key_n);
+
+            string result = RSA_Dedoce(sourcetext, d, n);
+
+            return result;
+        } //изменить
+
+        private List<string> RSA_Endoce(string sourcetext, long e, long n) //шифрование
+        {
+            List<string> result = new List<string>();
+
+            BigInteger bi;
+
+            for (int i = 0; i < sourcetext.Length; i++)
+            {
+                int index = Array.IndexOf(alph.lang, sourcetext[i]);
+
+                bi = new BigInteger(index);
+                bi = BigInteger.Pow(bi, (int)e);
+
+                BigInteger bn = new BigInteger((int)n);
+
+                bi = bi % bn;
+
+                result.Add(bi.ToString());
+            }
+
+            return result;
+        }
+
+        private string RSA_Dedoce(List<string> sourcetext, long d, long n) //дешифрование
+        {
+            string result = "";
+
+            BigInteger bi;
+
+            foreach (string item in sourcetext)
+            {
+                bi = new BigInteger(Convert.ToDouble(item));
+                bi = BigInteger.Pow(bi, (int)d);
+
+                BigInteger bn = new BigInteger((int)n);
+
+                bi = bi % bn;
+
+                int index = Convert.ToInt32(bi.ToString());
+
+                if (index == -1)
+                {
+                    result += " ";
+                }
+                else
+                {
+                    result += alph.lang[index].ToString();
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsTheNumberSimple(long n) //является ли число простым
+        {
+            if (n < 2)
+                return false;
+
+            if (n == 2)
+                return true;
+
+            for (int i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        private long Calculate_e(long fi) //вычисление открытой экспоненты
+        {
+            long e = 2;
+
+            for (int i = 2; i <= fi; i++)
+                if ((fi % i == 0) && (e % i == 0) && e < fi) //если имеют общие делители
+                {
+                    e++;
+                    i = 1;
+                }
+
+            return e;
+        }
+
+        private long Calculate_d(long e, long fi) //вычисление закрытой экспоненты 
+        {
+            long d = 2;
+
+            while (true)
+            {
+                if ((d * e % fi == 1) && d != e)
+                    break;
+                else
+                    d++;
+            }
+
+            return d;
+        }
+    }
+}
